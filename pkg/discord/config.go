4e315b9ec873b745f3db7a98997855611399d7e3
Adirelle <adirelle@gmail.com>
2022-04-21 13:27:24 +0200

    Removed not-yet-released code.

--- pkg/discord/config.go
+++ pkg/discord/config.go
@@ -6,12 +6,11 @@ import (
 
 type (
 	Config struct {
-		Token         utils.Secret       `json:"token" validate:"required"`
-		GuildID       Snowflake          `json:"serverId" validate:"required"`
-		ChannelIDs    []Snowflake        `json:"channelIds" validate:"required,min=1"`
-		CommandPrefix string             `json:"commandPrefix,omitempty" validate:"required,len=1"`
-		Permissions   *Permissions       `json:"permissions" validate:"required"`
-		Notifications NotificationConfig `json:"notify,omitempty"`
+		Token         utils.Secret `json:"token" validate:"required"`
+		GuildID       Snowflake    `json:"serverId" validate:"required"`
+		ChannelIDs    []Snowflake  `json:"channelIds" validate:"required,min=1"`
+		CommandPrefix string       `json:"commandPrefix,omitempty" validate:"required,len=1"`
+		Permissions   *Permissions `json:"permissions" validate:"required"`
 	}
 )
 
